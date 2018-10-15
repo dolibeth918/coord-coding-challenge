ff80d3218a778961e88f966610bfd7ee0ff6f30f
Dolibeth Estevez <dolibeth.estevez@gmail.com>
2018-10-15 19:17:55 -0400

    feat: app fetches data from coord api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  async componentDidMount() {
+    const res = await axios.get(
+      `https://api.staging.coord.co/codechallenge/commits`
+    );
+    const timestamps = res.data;
+    console.log(timestamps);
+  }
+
   render() {
     return (
       <div className="App">
