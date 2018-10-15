79bea1f5874d90f4f1b3c66ee5ab5eaaa0202b80
Dolibeth Estevez <dolibeth.estevez@gmail.com>
2018-10-15 19:25:26 -0400

    feat: 2000th commit estimation calcs

--- src/App.js
+++ src/App.js
@@ -3,12 +3,26 @@ import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      averageCommitTime: 0
+    };
+  }
   async componentDidMount() {
     const res = await axios.get(
       `https://api.staging.coord.co/codechallenge/commits`
     );
     const timestamps = res.data;
-    console.log(timestamps);
+    // current time passed from the first commit to the last
+    let secondsPassed = timestamps[0] - timestamps[timestamps.length - 1];
+    // average time per commit
+    let avg = secondsPassed / timestamps.length;
+    // how many commits are left
+    let commitsLeft = 2000 - timestamps.length;
+    // end time according to average commit time and commits left
+    let timeEnd = timestamps[0] + avg * commitsLeft;
+    console.log(timeEnd);
   }
 
   render() {
